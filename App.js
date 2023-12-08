35aa5626ca2bafded32669df7ea239f9db498472
FathimaAzma <140784165+FathimaAzma@users.noreply.github.com>
2023-12-08 12:08:27 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Content from './component/Content';
+import Footer from './component/Footer';
+import Header from './component/Header';
+
+
+function App() {
+  return (
+    <>
+    <Header></Header>
+    <Content></Content>
+    <hr></hr>
+    <Footer></Footer>
+    </>
+  )
+}
+
+export default App;
